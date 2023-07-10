1399d01ace571704f715003849223573c9aca316
Vukky <vukky@litdevs.org>
2023-07-10 23:58:37 +0200

    split disclaimers and proper fortunes in greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 function fish_greeting
     clear
-    echo -e "Welcome to fish! $(fortune /usr/share/fortune/disclaimer /usr/share/fortune/fortunes ~/.config/fortunes/quotes)\nfish $FISH_VERSION, Linux $(uname -r)"
+    echo -e "Welcome to fish! $(fortune /usr/share/fortune/disclaimer)\n$(fortune /usr/share/fortune/fortunes ~/.config/fortunes/quotes)\nfish $FISH_VERSION, Linux $(uname -r)"
 end
